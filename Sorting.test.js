c2aacd90c34dfeb8530a0b11f911fdf2378eff71
Aakash <csecec1803106@gmail.com>
2023-08-18 14:57:42 +0530

    Add Comments

--- Sorting.test.js
+++ Sorting.test.js
@@ -1,10 +1,10 @@
 const { expect } = require('chai');
-const { sortOddNumbers } = require('./sorting');
+const { sortOddNumbers } = require('./sorting'); // Import the module with your sorting function
 
 describe('sortOddNumbers', () => {
   it('should sort odd numbers while keeping even numbers in place', () => {
-    const input = [5, 3, 2, 8, 1, 4];
-    const expectedOutput = [1, 3, 2, 8, 5, 4];
+    const input = [5, 3, 2, 8, 1, 4]; // Example input array
+    const expectedOutput = [1, 3, 2, 8, 5, 4]; // Expected output array after sorting odd numbers
 
     const result = sortOddNumbers(input);
 
@@ -19,7 +19,7 @@ describe('sortOddNumbers', () => {
   });
 
   it('should keep even numbers in place', () => {
-    const input = [4, 8, 2, 6];
+    const input = [4, 8, 2, 6]; //Only even numbers
     const result = sortOddNumbers(input);
 
     expect(result).to.deep.equal(input);
